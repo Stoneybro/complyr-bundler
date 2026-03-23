6295a8bf411cea7fd3f92edae686d03e15784b14
stoneybro <zionlivingstone4@gmail.com>
2026-03-23 01:56:06 +0100

    increase vglmarkup to 6000

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ cat > /usr/app/config.json << CONF
   "entryPoints": ["0x0000000071727De22E5E9d8BAf0edAc6f37da032"],
   "relayers": ["${MNEMONIC}"],
   "rpcEndpoint": "https://testnet.evm.nodes.onflow.org",
-  "chainId": 545
+  "chainId": 545,
+  "vglMarkup": 60000
 }
 CONF
-node ./packages/cli/bin/skandha.js standalone --unsafeMode
+node ./packages/cli/bin/skandha.js standalone
