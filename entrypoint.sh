2d645e402245c7821bf645049112635a292e2526
stoneybro <zionlivingstone4@gmail.com>
2026-04-12 18:23:49 +0100

    feat: update RPC endpoint to HashKey Testnet

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@ cat > /usr/app/config.json << CONF
 {
   "entryPoints": ["0x0000000071727De22E5E9d8BAf0edAc6f37da032"],
   "relayers": ["${MNEMONIC}"],
-  "rpcEndpoint": "https://testnet.evm.nodes.onflow.org",
-  "chainId": 545,
+  "rpcEndpoint": "https://testnet.hsk.xyz",
+  "chainId": 133,
   "vglMarkup": 60000,
   "cglMarkup": 150000
 }
